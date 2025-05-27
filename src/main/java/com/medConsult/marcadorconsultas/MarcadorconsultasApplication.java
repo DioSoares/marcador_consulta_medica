3cdba2038243d1aaffc1f09113ba98f2e304fdc6
DioSoares <dioxsoares@gmail.com>
2025-05-27 11:20:47 -0300

    first commit

---
+++ src/main/java/com/medConsult/marcadorconsultas/MarcadorconsultasApplication.java
@@ -0,0 +1,13 @@
+package com.medConsult.marcadorconsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcadorconsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcadorconsultasApplication.class, args);
+	}
+
+}
